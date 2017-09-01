33c77a84eb536510ee9731bc5961b86480b1d1bf
Evan Lovely <evanlovely@gmail.com>
2017-09-01 11:44:19 -0700

    adding Twig Namespaces functionality

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,29 @@ const jsTasks = require('@theme-tools/plugin-js-concat-babel')(config.js);
 // const iconTasks = require('@theme-tools/plugin-icon-font')(config.icons);
 const patternLabTasks = require('@theme-tools/plugin-pattern-lab-php')({
   configFile: './pattern-lab/config/config.yml',
+  twigNamespaces: {
+    sets: [
+      {
+        namespace: 'base',
+        paths: ['pattern-lab/source/_patterns/00-base'],
+      }, {
+        namespace: 'atoms',
+        paths: ['pattern-lab/source/_patterns/01-atoms'],
+      }, {
+        namespace: 'molecules',
+        paths: ['pattern-lab/source/_patterns/02-molecules'],
+      }, {
+        namespace: 'organisms',
+        paths: ['pattern-lab/source/_patterns/03-organisms'],
+      }, {
+        namespace: 'templates',
+        paths: ['pattern-lab/source/_patterns/04-templates'],
+      }, {
+        namespace: 'pages',
+        paths: ['pattern-lab/source/_patterns/05-pages'],
+      },
+    ],
+  },
 });
 
 function scssToJsonTask(done) {
