c87832401775229ab851aad079582ac91e1e5d5c
Evan Lovely <EvanLovely@users.noreply.github.com>
2017-12-02 17:27:01 -0800

    Adding version checks

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 if [[ $IN_NETLIFY == 'yes' ]]; then
+  echo 'In Netlify, setting up php and composer.'
   source ~/.phpbrew/bashrc
   phpbrew switch `phpbrew list|tail -n1`
+  php -v
+  composer --version
 fi
