94e8124678db7d57997f2a430942496006fb9a77
Evan Lovely <EvanLovely@users.noreply.github.com>
2017-12-02 17:31:10 -0800

    troubleshooting

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ if [[ $IN_NETLIFY == 'yes' ]]; then
   echo 'In Netlify, setting up php and composer.'
   source ~/.phpbrew/bashrc
   phpbrew switch `phpbrew list|tail -n1`
-  php -v
-  composer --version
 fi
+
+cd pattern-lab
+composer install --no-interaction
