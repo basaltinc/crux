cae7bb72c39a81479f8fa9dd5da76c41e1b01825
Evan Lovely <EvanLovely@users.noreply.github.com>
2017-12-02 18:42:57 -0800

    changing around setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 if [[ $IN_NETLIFY == 'yes' ]]; then
-  echo 'In Netlify, setting up php and composer.'
+  echo 'In Netlify, setting up php and composer...'
   source ~/.phpbrew/bashrc
   phpbrew switch `phpbrew list|tail -n1`
+  php -v
 fi
 
 cd pattern-lab
 composer install --no-interaction
 cd ..
+echo 'setup.sh done.'
