cb90bc3b8e9d3f93a97c3ef0db3823238f79498e
marywarrington <mary@basalt.io>
2018-06-19 12:18:55 -0700

    define function to enable javascript linking on any element

--- pattern-lab/source/_patterns/00-styleguide/utilities/utilities.js
+++ pattern-lab/source/_patterns/00-styleguide/utilities/utilities.js
@@ -1,7 +1,31 @@
-function marysCoolFunction() {
-  document.body.classList.add('mary');
+function jsOpenLink() {
+
+  // find elements with class js-link-to
+  var el = document.getElementsByClassName('js-link-to');
+
+  // define function to grab URL from data-linkto, open in new tab
+  function jsLinkClicked() {
+    var url = this.getAttribute('data-linkto');
+    window.open(url, '_blank');
+  }
+
+  // find any anchor tags within the clickable parent
+  var childLinks = document.querySelectorAll('.js-link-to a');
+
+  // prevent children from executing the parent click event
+  Array.from(childLinks).forEach(function(link){
+    link.addEventListener('click', function(e){
+      e.stopPropagation();
+    });
+  });
+
+  // attach click event listener to the parent
+  Array.from(el).forEach(function(element) {
+    element.addEventListener('click', jsLinkClicked);
+    console.log(childLinks);
+  });
 }
 
 module.exports = () => {
-  marysCoolFunction();
+  jsOpenLink();
 }
