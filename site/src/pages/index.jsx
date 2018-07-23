a194e0d4e765a8d701669b9d603c61979272daca
Joseph Karasek <joseph.karasek@gmail.com>
2018-07-23 10:52:11 -0700

    home landing page

--- site/src/pages/index.jsx
+++ site/src/pages/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
 import Header from '../components/header';
+import HomeSplash from '../components/home-splash';
 
 import './index.css';
 
 const IndexPage = () => (
-  <div>
-    <Header siteTitle={'Basalt Design System'}/>
-    <div className="page__main">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam consequatur cupiditate dignissimos dolor, eaque illo impedit ipsum, iusto natus nisi quasi quis quo rerum sed sint tempora voluptate voluptatem!</p>
+  <div className="app">
+    <Header className="app__header" siteTitle={'Basalt Design System'}/>
+    <div className="app__body">
+      <HomeSplash></HomeSplash>
     </div>
+    <div className="app__footer"></div>
   </div>
 );
 
