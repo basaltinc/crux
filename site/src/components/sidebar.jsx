ebbd3deda6e33d7a151e32903a72872dc851d734
Joseph Karasek <joseph.karasek@gmail.com>
2018-07-24 10:53:11 -0700

    update sidebare

--- site/src/components/sidebar.jsx
+++ site/src/components/sidebar.jsx
@@ -17,16 +17,25 @@ const SideBar = ({ files }) => {
   const VisualLanguageLinks = files
     .filter(file => file.node.frontmatter.section === 'visual-language')
     .map(file => (<SideBarLink file={file.node} />));
+  const GetStartedLinks = files
+    .filter(file => file.node.frontmatter.section === 'get-started')
+    .map(file => (<SideBarLink file={file.node} />));
+  const ComponentLinks = files
+    .filter(file => file.node.frontmatter.section === 'components')
+    .map(file => (<SideBarLink file={file.node} />));
   // const GeneralLinks = files
   //   .filter(file => file.node.frontmatter.section === 'general')
   //   .map(file => (<SideBarLink file={file.node} />));
 
   return (
     <div className="sidebar">
-      <h4>General</h4>
       <ul>{GeneralLinks}</ul>
+      <h4>Get Started</h4>
+      <ul>{GetStartedLinks}</ul>
       <h4>Visual Language</h4>
       <ul>{VisualLanguageLinks}</ul>
+      <h4>Components</h4>
+      <ul>{ComponentLinks}</ul>
     </div>
   );
 }
