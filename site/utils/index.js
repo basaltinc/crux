b27c18e511054840f9dae675b4a4275d97464c50
Evan Lovely <evanlovely@gmail.com>
2018-07-25 14:33:36 -0700

    Adding utilities

---
+++ site/utils/index.js
@@ -0,0 +1,11 @@
+export * from './color';
+export * from './arrays';
+
+/**
+ * Get Random Integer
+ * @param {number} max - Highest number
+ * @returns {number} - A random integer
+ */
+export function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
