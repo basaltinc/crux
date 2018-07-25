3400f45fdbf5f8b2ebd15e18ac6e7d045971fb22
Joseph Karasek <joseph.karasek@gmail.com>
2018-07-25 11:38:41 -0700

    page component takes className through for custom styling

--- site/src/templates/page.jsx
+++ site/src/templates/page.jsx
@@ -8,25 +8,29 @@ import './page.css';
 // @todo Evan is working on a better way to pull in styles
 import '../../../build/assets/style.css'; // eslint-disable-line
 
-const Page = props => (
-  <Site>
-    <main className="page">
-      <div className="page__content">
-        {props.children}
-      </div>
-      {props.sidebarOne && (
-        <div className="page__sidebar-one">
-          {props.sidebarOne}
+const Page = props => {
+  let mainClassName = props.className ? 'page '.concat(props.className) : 'page';
+  return (
+    <Site>
+      <main className={mainClassName}>
+        <div className="page__content">
+          {props.children}
         </div>
-      )}
-      {props.sidebarTwo && (
-        <div className="page__sidebar-two">
-          {props.sidebarTwo}
-        </div>
-      )}
-    </main>
-  </Site>
-);
+        {props.sidebarOne && (
+          <div className="page__sidebar-one">
+            {props.sidebarOne}
+          </div>
+        )}
+        {props.sidebarTwo && (
+          <div className="page__sidebar-two">
+            {props.sidebarTwo}
+          </div>
+        )}
+      </main>
+    </Site>
+  );
+}
+
 
 Page.propTypes = {
   children: PropTypes.node.isRequired,
