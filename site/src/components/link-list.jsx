b1919b1ab94552b4a33b892a1e3bb288da2a68f5
Joseph Karasek <joseph.karasek@gmail.com>
2018-07-26 13:54:30 -0700

    add custom links to pattern page

--- site/src/components/link-list.jsx
+++ site/src/components/link-list.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 
-const LinkList = ({ items }) => (
-  <nav className="link-list">
-    <ul>
-      {items.map(item => (
-        <li key={item.id}>
-          <Link to={item.path}>{item.name}</Link>
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
+const LinkList = ({ items }) => {
+  console.log(items);
+  return (
+    <nav className="link-list">
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>
+            <Link to={item.path}>{item.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 LinkList.propTypes = {
   items: PropTypes.arrayOf(
--- site/src/templates/pattern-page.jsx
+++ site/src/templates/pattern-page.jsx
@@ -4,21 +4,53 @@ import Page from './page';
 import LinkList from '../components/link-list';
 import { components } from '../../data';
 
-const PatternPage = props => (
-  <Page
-    sidebarOne={
-      <LinkList
-        items={components.map(component => ({
+// @todo think of a way to not have these be hard coded, then implement that brilliant idea
+const perceptualPatternsList = [
+  {
+    name: 'Colors',
+    id: 'colors',
+    path: `/patterns/colors`,
+  },
+  {
+    name: 'Animations',
+    id: 'animations',
+    path: `/patterns/animations`,
+  },
+  {
+    name: 'Spacings',
+    id: 'spacings',
+    path: '/patterns/spacings',
+  },
+  {
+    name: 'Components',
+    id: 'components',
+    isHeading: true,
+  },
+];
+
+const PatternPage = props => {
+  const linkItems = perceptualPatternsList
+    .concat(
+      components
+        .map(component => ({
           name: component.title,
           id: component.id,
           path: `/patterns/components/${component.id}`,
-        }))}
-      />
-    }
-  >
-    {props.children}
-  </Page>
-);
+        }))
+    );
+
+  return (
+    <Page
+      sidebarOne={
+        <LinkList
+          items={linkItems}
+        />
+      }
+    >
+      {props.children}
+    </Page>
+  );
+}
 
 PatternPage.propTypes = {
   children: PropTypes.node.isRequired,
