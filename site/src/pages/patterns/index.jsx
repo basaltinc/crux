0af1c45536cdc912976ddf06aab899b658712335
Evan Lovely <evanlovely@gmail.com>
2018-07-27 15:55:37 -0700

    Adding getPatternInfo and getPatterns api endpoints and refactoring site to use

--- site/src/pages/patterns/index.jsx
+++ site/src/pages/patterns/index.jsx
@@ -1,38 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'gatsby-link';
 import Page from '../../templates/page';
-import { patterns } from '../../../data';
+import Spinner from '../../components/spinner';
+import { apiUrlBase } from '../../../config';
+import mediaBlockImage from './media-block.svg';
+import mediaTileImage from './media-tile.svg';
+import heroImage from './hero.svg';
 
-const PatternsPage = () => (
-  <Page>
-    <h2>Welcome to the Design System!</h2>
-    <ul
-      className="smart-grid u-crux-pattern-grid"
-      data-row-items-small="2"
-      data-row-items-medium="3"
-    >
-      {patterns.map(pattern => (
-        <li
-          key={pattern.id}
-          className="u-crux-pattern-grid__item u-crux-pattern-grid__item--component"
+// @todo refactor
+const images = {
+  hero: heroImage,
+  'media-tile': mediaTileImage,
+  'media-block': mediaBlockImage,
+};
+
+class PatternsPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      patterns: [],
+      ready: false,
+    };
+  }
+
+  componentDidMount() {
+    window
+      .fetch(`${apiUrlBase}/patterns/components`)
+      .then(res => res.json())
+      .then(patterns => {
+        this.setState({ patterns, ready: true });
+      });
+  }
+
+  render() {
+    let content;
+    if (!this.state.ready) {
+      content = <Spinner />;
+    } else {
+      const items = this.state.patterns.map(pattern => {
+        const image = images[pattern.id];
+        return (
+          <li
+            key={pattern.id}
+            className="u-crux-pattern-grid__item u-crux-pattern-grid__item--component"
+          >
+            <Link to={pattern.path}>
+              {image && (
+                <img
+                  src={image}
+                  alt=""
+                  className="u-crux-pattern-grid__item-thumb"
+                />
+              )}
+              <span className="u-crux-pattern-grid__item-title">
+                {pattern.title}
+              </span>
+              <div className="u-crux-pattern-grid__item-description">
+                {pattern.schema.description}
+              </div>
+            </Link>
+          </li>
+        );
+      });
+      content = (
+        <ul
+          className="smart-grid u-crux-pattern-grid"
+          data-row-items-small="2"
+          data-row-items-medium="3"
         >
-          <Link to={pattern.path}>
-            <img
-              src={pattern.image}
-              alt=""
-              className="u-crux-pattern-grid__item-thumb"
-            />
-            <span className="u-crux-pattern-grid__item-title">
-              {pattern.title}
-            </span>
-            <div className="u-crux-pattern-grid__item-description">
-              {pattern.data.desc}
-            </div>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </Page>
-);
+          {items}
+        </ul>
+      );
+    }
+    return (
+      <Page>
+        <h2>Welcome to the Design System!</h2>
+        {content}
+      </Page>
+    );
+  }
+}
 
 export default PatternsPage;
