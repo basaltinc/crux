0d11c00e9dad40bc0740dbdd726f1deb5fff6a1c
marywarrington <mary@basalt.io>
2018-07-30 16:03:37 -0700

    add color converting select

--- site/src/bedrock/components/color-swatch.jsx
+++ site/src/bedrock/components/color-swatch.jsx
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { convertColor } from '../packages/utils';
 
-const ColorSwatch = ({ color }) => (
-  <div
-    style={{
-      width: '49%',
-      marginBottom: '10px',
-      padding: '5px',
-      border: 'solid 1px grey',
-    }}
-  >
-    Color: <code>{color.name}</code>
-    <br />
-    Value: <code>{color.value}</code>
+const ColorSwatch = ({ color, format }) => {
+  const colorValue = convertColor(color.value, format);
+  return (
     <div
       style={{
-        height: '50px',
-        backgroundColor: color.value,
-        border: 'dashed 1px grey',
+        width: '49%',
+        marginBottom: '10px',
+        padding: '5px',
+        border: 'solid 1px grey',
       }}
-    />
-  </div>
-);
+    >
+      Color: <code>{color.name}</code>
+      <br />
+      Value: <code>{colorValue}</code>
+      <div
+        style={{
+          height: '50px',
+          backgroundColor: colorValue,
+          border: 'dashed 1px grey',
+        }}
+      />
+    </div>
+  );
+};
 
 ColorSwatch.propTypes = {
+  format: PropTypes.string.isRequired,
   color: PropTypes.shape({
     name: PropTypes.string.isRequired,
     value: PropTypes.string.isRequired,
@@ -31,24 +36,53 @@ ColorSwatch.propTypes = {
   }).isRequired,
 };
 
-/* eslint-disable no-useless-constructor, react/prefer-stateless-function */
-const ColorSwatches = props => {
-  const colorSwatches = props.colors.map(color => (
-    <ColorSwatch key={color.name} color={color} />
-  ));
+class ColorSwatches extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      format: 'hsl',
+    };
+  }
 
-  return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        flexWrap: 'wrap',
-      }}
-    >
-      {colorSwatches}
-    </div>
-  );
-};
+  render() {
+    const options = ['hsl', 'rgb', 'hex'].map(option => (
+      <option value={option} key={option}>
+        {option}
+      </option>
+    ));
+    const colorSwatches = this.props.colors.map(color => (
+      <ColorSwatch key={color.name} color={color} format={this.state.format} />
+    ));
+    return (
+      <div>
+        <label
+          style={{
+            textAlign: 'right',
+            width: '100%',
+            display: 'block',
+          }}
+        >
+          Color Format:
+          <select
+            value={this.state.value}
+            onChange={event => this.setState({ format: event.target.value })}
+          >
+            {options}
+          </select>
+        </label>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            flexWrap: 'wrap',
+          }}
+        >
+          {colorSwatches}
+        </div>
+      </div>
+    );
+  }
+}
 
 ColorSwatches.propTypes = {
   colors: PropTypes.arrayOf(PropTypes.object).isRequired,
