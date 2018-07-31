feccbed98f2ef894c7bc63222b4b00b4387a19fb
Joseph Karasek <joseph.karasek@gmail.com>
2018-07-30 17:52:23 -0700

    move styling of components board out of css

--- site/src/components/component-board.jsx
+++ site/src/components/component-board.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import Spinner from '../bedrock/components/spinner';
-import './component-board.css';
 
 import mediaBlockImage from '../../../images/component-thumbnails/media-block.svg';
 import mediaTileImage from '../../../images/component-thumbnails/media-tile.svg';
@@ -17,42 +16,122 @@ const images = {
   'media-block': mediaBlockImage,
 };
 
+const PatternGrid = styled.ul`
+  margin: 50px;
+  padding: 0;
+  list-style-type: none;
+  align-items: flex-start;
+`;
+
+const PatternGridItem = styled.li`
+  background: #fff;
+  position: relative;
+  transition: all 0.2s ease-in-out;
+  ${PatternGrid}:hover & {
+    filter: blur(2px);
+  }
+  &:hover {
+    filter: blur(0px) !important;
+    z-index: 1;
+  }
+  > a:link,
+  > a:visited {
+    display: block;
+    font-family: 'AvenirMedium', sans-serif;
+    text-decoration: none;
+    padding: 30px 30px 0;
+    height: 100%;
+    border: solid 1px #ccc;
+    border-radius: var(--border-radius);
+    text-align: center;
+    transition: all 0.2s ease-in-out;
+    position: relative;
+    z-index: 1;
+  }
+  > a:hover {
+    position: absolute;
+    background: #fff;
+    border-radius: var(--border-radius);
+    transform: scale(1.25);
+    width: 100%;
+    height: auto;
+    z-index: 2;
+  }
+`;
+
+const PatternGridItemThumb = styled.img`
+  display: block;
+  max-width: 250px;
+  width: 100%;
+  max-height: 110px;
+  margin: 0 auto 15px;
+  filter: grayscale(75%);
+  transition: all 0.2s ease-in-out;
+  ${PatternGridItem}:hover & {
+    filter: grayscale(0%);
+  }
+`;
+
+const PatternGridItemTitle = styled.span`
+  color: #000;
+  margin-bottom: -3px;
+  border-bottom: 5px solid #000;
+  display: inline-block;
+  transition: all 0.2s ease-in-out;
+`;
+
+const PatternGridItemDescription = styled.div`
+  font-family: 'AvenirLight', sans-serif;
+  line-height: 1.25;
+  font-size: 14px;
+  color: #000;
+  font-style: italic;
+  padding: 25px 0 25px;
+  max-width: 250px;
+  margin: 0 auto;
+  opacity: 0;
+  height: 0px;
+  pointer-events: none;
+  transition: all 0.1s ease-in-out;
+  transform: scale(0.75);
+  position: absolute;
+
+  ${PatternGridItem} > a:hover & {
+    opacity: 1;
+    transform: scale(1);
+    height: auto;
+    position: relative;
+  }
+`;
+
 export default function ComponentsBoard(props) {
   if (!props.ready) {
     return <Spinner />;
   }
   return (
-    <ul
-      className="smart-grid u-crux-pattern-grid"
+    <PatternGrid
+      className="smart-grid"
       data-row-items-small="2"
       data-row-items-medium="3"
     >
       {props.patterns.map(pattern => {
         const image = images[pattern.id];
         return (
-          <li
+          <PatternGridItem
             key={pattern.id}
-            className="u-crux-pattern-grid__item u-crux-pattern-grid__item--component"
+            className="u-crux-pattern-grid__item--component"
           >
             <Link to={pattern.path}>
-              {image && (
-                <img
-                  src={image}
-                  alt=""
-                  className="u-crux-pattern-grid__item-thumb"
-                />
-              )}
-              <span className="u-crux-pattern-grid__item-title">
-                {pattern.title}
-              </span>
-              <div className="u-crux-pattern-grid__item-description">
+              {image && <PatternGridItemThumb src={image} alt="" />}
+              <PatternGridItemTitle>{pattern.title}</PatternGridItemTitle>
+              <PatternGridItemDescription>
                 {pattern.schema.description}
-              </div>
+              </PatternGridItemDescription>
             </Link>
-          </li>
+          </PatternGridItem>
         );
       })}
-    </ul>
+    </PatternGrid>
   );
 }
 
