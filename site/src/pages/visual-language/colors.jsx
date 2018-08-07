f4b2db21f80de63c83a21d218b52d016339c89ae
marywarrington <mary@basalt.io>
2018-08-07 10:34:53 -0700

    implement dos and donts on colors page

--- site/src/pages/visual-language/colors.jsx
+++ site/src/pages/visual-language/colors.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 import VisualLanguagePage from '../../templates/visual-language-page';
 import ColorSwatches from '../../bedrock/components/color-swatch';
+import DosAndDonts from '../../bedrock/components/dos-and-donts';
 import { apiUrlBase } from '../../../config';
 import ColorContrastBlock from '../../bedrock/components/color-contrast-block';
+import exampleDo from '../../../../images/dos-and-donts/background-colors/background-colors-do.png';
+import exampleDont from '../../../../images/dos-and-donts/background-colors/background-colors-dont.png';
 
 class ColorsPage extends React.Component {
   constructor(props) {
@@ -34,6 +37,21 @@ class ColorsPage extends React.Component {
             can speak to the soul in a thousand different ways. - Oscar Wilde
           </blockquote>
           <hr />
+          <DosAndDonts
+            items={[
+              {
+                image: exampleDont,
+                caption: 'use color combinations that fail WCAG standards.',
+                do: false,
+              },
+              {
+                title: 'Do Example',
+                image: exampleDo,
+                caption: 'use color combinations with distinct contrast that fulfill WCAG standards.',
+                do: true,
+              },
+            ]}
+          />
           <p>
             Color is a defining element of any strong brand identity. Dedicated
             use of the Basalt palette with reinforce the cohesiveness of the
