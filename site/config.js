98c6668037769001efe904f425ab02a77a4a4d50
Evan Lovely <evanlovely@gmail.com>
2018-08-08 15:02:04 -0700

    Setting up dynamic api base

--- site/config.js
+++ site/config.js
@@ -1,4 +1,3 @@
-// export const apiUrlBase = process.env.apiUrlBase
-//   ? process.env.apiUrlBase
-//   : 'http://localhost:3042/api'; // @todo make dynamic
-export const apiUrlBase = '/api'; // @todo make dynamic
+export const apiUrlBase = process.env.apiUrlBase
+  ? process.env.apiUrlBase
+  : 'http://localhost:3042/api';
