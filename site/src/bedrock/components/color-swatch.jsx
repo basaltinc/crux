be0ed46321bad0a2605c7712a9d2e7ca0a265a50
Joseph Karasek <joseph.karasek@gmail.com>
2018-08-08 10:53:04 -0700

    add proper copy to clipboard for typography and colors

--- site/src/bedrock/components/color-swatch.jsx
+++ site/src/bedrock/components/color-swatch.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { convertColor } from '../packages/utils';
 
 const OuterSwatch = styled.div`
@@ -28,6 +29,10 @@ const SwatchesWrapper = styled.div`
   flex-wrap: wrap;
 `;
 
+const CopyToClipboardWrapper = styled.div`
+  float: right;
+`;
+
 const ColorSwatch = ({ color, format }) => {
   const colorValue = convertColor(color.value, format);
 
@@ -36,6 +41,21 @@ const ColorSwatch = ({ color, format }) => {
       Color: <code>{color.name}</code>
       <br />
       Value: <code>{colorValue}</code>
+      <CopyToClipboardWrapper>
+        <CopyToClipboard
+          text={colorValue}
+          onCopy={() => window.alert(`${colorValue} copied to clipboard`)}
+        >
+          <svg
+            width="26"
+            height="26"
+            viewBox="0 0 1792 1792"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M1696 384q40 0 68 28t28 68v1216q0 40-28 68t-68 28h-960q-40 0-68-28t-28-68v-288h-544q-40 0-68-28t-28-68v-672q0-40 20-88t48-76l408-408q28-28 76-48t88-20h416q40 0 68 28t28 68v328q68-40 128-40h416zm-544 213l-299 299h299v-299zm-640-384l-299 299h299v-299zm196 647l316-316v-416h-384v416q0 40-28 68t-68 28h-416v640h512v-256q0-40 20-88t48-76zm956 804v-1152h-384v416q0 40-28 68t-68 28h-416v640h896z" />
+          </svg>
+        </CopyToClipboard>
+      </CopyToClipboardWrapper>
       <InnerSwatch colorValue={color.value} />
     </OuterSwatch>
   );
