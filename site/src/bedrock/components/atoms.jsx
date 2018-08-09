a2bb01e9ab49d3847fda044a35fd0d7371304e1f
Jake Love <jake@basalt.io>
2018-08-08 17:30:34 -0700

    Atoms - added the 'Checkerboard' atom

--- site/src/bedrock/components/atoms.jsx
+++ site/src/bedrock/components/atoms.jsx
@@ -8,8 +8,29 @@ export const Details = styled.details`
   summary {
     font-weight: bold;
     font-size: 125%;
+    outline: none;
+    user-select: none;
   }
   &:hover {
     cursor: pointer;
   }
 `;
+
+export const Checkerboard = styled.div`
+  background-image: linear-gradient(
+      45deg,
+      rgb(230, 230, 230) 25%,
+      transparent 25%
+    ),
+    linear-gradient(-45deg, rgb(230, 230, 230) 25%, transparent 25%),
+    linear-gradient(45deg, transparent 75%, rgb(230, 230, 230) 75%),
+    linear-gradient(-45deg, transparent 75%, rgb(230, 230, 230) 75%);
+  background-size: 20px 20px;
+  background-position: 0px 0px, 0px 10px, 10px -10px, -10px 0px;
+  padding: ${props => props.bleed && props.bleed};
+  > div {
+    border: 10px solid rgba(77, 77, 77, 0.15);
+  }
+`;
+
+export default Checkerboard;
