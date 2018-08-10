323375e072c7427d57ec9e349ead88761510cbe2
Joseph Karasek <joseph.karasek@gmail.com>
2018-08-10 12:10:03 -0700

    move in sidebar start

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -6,6 +6,9 @@ import Header from './components/header';
 import Footer from './components/footer';
 import './global.css';
 import ErrorCatcher from './bedrock/components/error-catcher';
+import { apiUrlBase } from '../config';
+import { TypeToFilterWrapper } from './bedrock/components/atoms';
+import LinkList from './components/link-list';
 
 const Page1 = () => <div>ima Page1</div>;
 const Page2 = () => <div>ima Page2</div>;
@@ -24,13 +27,128 @@ const SiteFooter = styled.div`
   }
 `;
 
+const SidebarStyled = styled.div`
+  width: 10%;
+  flex-grow: 0;
+  flex-shrink: 0;
+  min-width: 300px;
+  max-width: 350px;
+  padding: 2rem;
+  border-right: solid 1px lightgrey;
+  background-color: #f2f3f3;
+  h4 {
+    margin: 1.25rem 0 0.25rem;
+  }
+  ul {
+    list-style: none;
+    padding-left: 0;
+    margin: 0;
+  }
+`;
+
+const SidebarOneStyled = SidebarStyled.extend`
+  order: -1;
+  ${props =>
+    props.sidebarOneOnTop
+      ? `
+    width: 100%;
+    max-width: 100%;
+    border-right: 0;
+    border-bottom: solid 1px black;
+    ul {
+      display: flex;
+      align-items: center;
+      li {
+        margin: 5px 5px 5px 0;
+      }
+      * {
+        margin: 0;
+      }
+    }
+  `
+      : ''};
+`;
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      sidebarOneOnTop: false,
+      filteredList: [
+        {
+          name: 'Patterns',
+          id: 'patterns',
+          isHeading: true,
+        },
+      ],
+      linkItems: [
+        {
+          name: 'Patterns',
+          id: 'patterns',
+          isHeading: true,
+        },
+      ],
+      ready: false,
+    };
+
+    this.handleFilterList = this.handleFilterList.bind(this);
+    this.filterableList = this.filterableList.bind(this);
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    window
+      .fetch(`${apiUrlBase}/patterns/components`)
+      .then(res => res.json())
+      .then(patterns => {
+        this.setState({
+          linkItems: [
+            ...this.state.linkItems,
+            ...patterns.map(pattern => ({
+              name: pattern.title,
+              id: pattern.id,
+              path: pattern.path,
+            })),
+          ],
+          filteredList: [
+            ...this.state.linkItems,
+            ...patterns.map(pattern => ({
+              name: pattern.title,
+              id: pattern.id,
+              path: pattern.path,
+            })),
+          ],
+        });
+      });
+  }
+
+  handleFilterList(event) {
+    const updatedList = this.state.linkItems.filter(
+      item =>
+        item.name.toLowerCase().search(event.target.value.toLowerCase()) !==
+          -1 || item.isHeading,
+    );
+    this.setState({
+      filteredList: updatedList,
+      filterTerm: event.target.value,
+    });
+  }
+
+  filterableList() {
+    return (
+      <div>
+        <TypeToFilterWrapper>
+          <input
+            type="text"
+            className="type-to-filter"
+            placeholder="Type to filter..."
+            onChange={this.handleFilterList}
+          />
+        </TypeToFilterWrapper>
+        <LinkList items={this.state.filteredList} />
+      </div>
+    );
+  }
 
   render() {
     return (
@@ -39,7 +157,19 @@ export default class App extends React.Component {
           <div>
             <Header siteTitle={'Crux'} />
             <SiteMain>
-              <div>Sidebar Temp</div>
+              <SidebarOneStyled {...this.state}>
+                {this.filterableList()}
+                <button
+                  className="button button--color-blue--light button--size-small"
+                  onClick={() =>
+                    this.setState({
+                      sidebarOneOnTop: !this.state.sidebarOneOnTop,
+                    })
+                  }
+                >
+                  Toggle
+                </button>
+              </SidebarOneStyled>
               <Switch>
                 <Route path="/page1" component={Page1} />
                 <Route path="/page2" component={Page2} />
@@ -60,7 +190,7 @@ export default class App extends React.Component {
               </Switch>
             </SiteMain>
             <SiteFooter>
-              <Footer></Footer>
+              <Footer />
             </SiteFooter>
           </div>
         </Router>
