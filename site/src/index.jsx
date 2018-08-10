4b34f5ba99cf00ac134e8be5005affffefbfbf07
Evan Lovely <evanlovely@gmail.com>
2018-08-10 10:43:36 -0700

    cleanup

--- site/src/index.jsx
+++ site/src/index.jsx
@@ -3,6 +3,5 @@ import ReactDom from 'react-dom';
 import App from './App';
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('loaded');
   ReactDom.render(<App />, document.getElementById('app'));
 });
