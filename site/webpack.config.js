d58245ce524872a156a737c4c9200d1c2af0984f
Joseph Karasek <joseph.karasek@gmail.com>
2018-08-10 16:19:09 -0700

    add png to webpack bundling

--- site/webpack.config.js
+++ site/webpack.config.js
@@ -15,7 +15,7 @@ const config = {
         exclude: /(node_modules)/,
       },
       {
-        test: [/\.jpeg?$/, /\.jpg?$/, /\.svg?$/],
+        test: [/\.jpeg?$/, /\.jpg?$/, /\.svg?$/, /\.png?$/],
         loader: 'url-loader',
       },
       {
