bf6adae7eb15c182da3f63a1bbb5ec89be27260f
marywarrington <mary@basalt.io>
2018-08-14 13:51:32 -0700

    add basic media block schema and template

--- pattern-lab/source/_patterns/03-components/media-block/index.js
+++ pattern-lab/source/_patterns/03-components/media-block/index.js
@@ -1,4 +1,5 @@
 const schema = require('./media-block-schema');
+const basicSchema = require('./media-block--basic.schema');
 
 const meta = {
   id: 'media-block',
@@ -6,6 +7,11 @@ const meta = {
   type: 'component',
   description: 'Image, Title, Body, and Buttons make this really flexible.',
   templates: [
+    {
+      name: '@components/_media-block--basic.twig',
+      selector: '.media-block--basic',
+      schema: basicSchema,
+    },
     {
       name: '@components/_media-block.twig',
       selector: '.media-block',
