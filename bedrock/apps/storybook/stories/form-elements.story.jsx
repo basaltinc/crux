6322050dc92105dddfb639879c8c122b83ab0483
Joseph Karasek <joseph.karasek@gmail.com>
2018-08-16 14:29:04 -0700

    start form elements, select dropdown

--- bedrock/apps/storybook/stories/form-elements.story.jsx
+++ bedrock/apps/storybook/stories/form-elements.story.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import SchemaForm from '@basalt/bedrock-schema-form';
+import { Details, Select } from '@basalt/bedrock-atoms';
+import { action } from '@storybook/addon-actions';
 
 const dummyData = {
   $schema: 'http://json-schema.org/draft-07/schema',
@@ -93,7 +95,25 @@ const dummyData = {
       enum: ['s', 'm', 'l', 'xl'],
       enumNames: ['Small', 'Medium', 'Large', 'Extra large'],
     },
-  }
+  },
 };
 
-storiesOf('Schema Form', module).add('Overview', () => <SchemaForm schema={dummyData}/>);
+storiesOf('Form Elements', module).add('Select', () => (
+  <Select
+    handleChange={action('option changed')}
+    items={[
+      {
+        title: 'Option 1',
+        value: 'option1',
+      },
+      {
+        title: 'Option 2',
+        value: 'option2',
+      },
+      {
+        title: 'Option 3',
+        value: 'option3',
+      },
+    ]}
+  />
+));
