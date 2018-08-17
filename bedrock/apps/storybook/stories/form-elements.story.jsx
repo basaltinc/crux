38e5507e32dff522808d74dfddf50812365d9445
Joseph Karasek <joseph.karasek@gmail.com>
2018-08-17 14:16:57 -0700

    custom text input and select into forms

--- bedrock/apps/storybook/stories/form-elements.story.jsx
+++ bedrock/apps/storybook/stories/form-elements.story.jsx
@@ -1,101 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { TextInput, TextAreaInput, Select } from '@basalt/bedrock-atoms';
+import { TextInputWrapper, Select } from '@basalt/bedrock-atoms';
 import { action } from '@storybook/addon-actions';
-
-const dummyData = {
-  $schema: 'http://json-schema.org/draft-07/schema',
-  type: 'object',
-  title: 'Media Block',
-  description: 'Image, Title, Body, and Buttons make this really flexible.',
-  properties: {
-    title: {
-      title: 'Title',
-      type: 'string',
-      description: 'Main heading of media block.',
-    },
-    inset_title: {
-      title: 'Inset Title',
-      type: 'boolean',
-      description: 'Insets the title.',
-    },
-    title_bg_color: {
-      title: 'Title Background Color',
-      type: 'string',
-      description: 'Any utility color class.',
-    },
-    title_text_color: {
-      title: 'Title Text Color',
-      description: 'Any utility color class.',
-      type: 'string',
-    },
-    body: {
-      title: 'Body Content',
-      type: 'string',
-      description: 'Body text of the media block.',
-    },
-    media: {
-      title: 'Media',
-      type: 'string',
-      description: 'Path to image.',
-    },
-    show_full_media: {
-      title: 'Show Full Media',
-      type: 'boolean',
-      description:
-        "Show full image. Default is to 'cover' the whole side the media item is on: this allows matching the height of the text but does not show full media item.",
-    },
-    media_position: {
-      title: 'Media Position',
-      type: 'string',
-      description:
-        'When Show Full Media is on, this positions the background image',
-      enum: [
-        'top-left',
-        'top-center',
-        'top-right',
-        'bottom-left',
-        'bottom-center',
-        'bottom-right',
-      ],
-      enumNames: [
-        'Top Left',
-        'Top Center',
-        'Top Right',
-        'Bottom Left',
-        'Bottom Center',
-        'Bottom Right',
-      ],
-    },
-    media_alignment: {
-      title: 'Media Alignment',
-      type: 'string',
-      enum: ['left', 'right', 'top', 'bottom'],
-      enumNames: ['Left', 'Right', 'Top', 'Bottom'],
-    },
-    media_padding: {
-      title: 'Media Padding',
-      type: 'string',
-      description: 'Space around the media',
-      enum: ['0', 's', 'm', 'l', 'xl', 'xxl'],
-      enumNames: [
-        'None',
-        'Small',
-        'Medium',
-        'Large',
-        'Extra Large',
-        'XX Large',
-      ],
-    },
-    media_size: {
-      title: 'Media Size',
-      type: 'string',
-      description: 'Size of media compared to content',
-      enum: ['s', 'm', 'l', 'xl'],
-      enumNames: ['Small', 'Medium', 'Large', 'Extra large'],
-    },
-  },
-};
 
 storiesOf('Form Elements', module)
   .add('Select', () => (
@@ -117,6 +23,14 @@ storiesOf('Form Elements', module)
       ]}
     />
   ))
-  .add('Text Input', () => <TextInput placeholder="Type here..." />)
-  .add('Text Area Input', () => <TextAreaInput placeholder="Type here..." />);
+  .add('Text Input', () => (
+    <TextInputWrapper>
+      <input placeholder="Type here..." />
+    </TextInputWrapper>
+  ))
+  .add('Text Area Input', () => (
+    <TextInputWrapper>
+      <textarea placeholder="Type here..." />
+    </TextInputWrapper>
+  ));
   // .add('Text Area Input', () => <TextAreaInput placeholder="Type here..." />);
--- bedrock/apps/storybook/stories/schema-form.story.jsx
+++ bedrock/apps/storybook/stories/schema-form.story.jsx
@@ -2,53 +2,28 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import SchemaForm from '@basalt/bedrock-schema-form';
 
+
 const dummyData = {
   $schema: 'http://json-schema.org/draft-07/schema',
   type: 'object',
-  title: 'Sample Schema',
-  description: 'This schema samples the various data structures definable through Json Schema.',
+  title: 'Schema Form Example',
+  description: 'Sample Schema for demo of React Json Schema Form Styling.',
   properties: {
-    string: {
+    title: {
       title: 'String',
       type: 'string',
-      description: 'A simple string.',
+      description: 'Description of a string.',
     },
-    inset_title: {
-      title: 'Inset Title',
+    boolean: {
+      title: 'Boolean',
       type: 'boolean',
-      description: 'Insets the title.',
+      description: 'Description of a boolean.',
     },
-    title_bg_color: {
-      title: 'Title Background Color',
-      type: 'string',
-      description: 'Any utility color class.',
-    },
-    title_text_color: {
-      title: 'Title Text Color',
-      description: 'Any utility color class.',
-      type: 'string',
-    },
-    body: {
-      title: 'Body Content',
-      type: 'string',
-      description: 'Body text of the media block.',
-    },
-    media: {
-      title: 'Media',
-      type: 'string',
-      description: 'Path to image.',
-    },
-    show_full_media: {
-      title: 'Show Full Media',
-      type: 'boolean',
-      description:
-        "Show full image. Default is to 'cover' the whole side the media item is on: this allows matching the height of the text but does not show full media item.",
-    },
-    media_position: {
-      title: 'Media Position',
+    string_enum: {
+      title: 'String w/ Enum',
       type: 'string',
       description:
-        'When Show Full Media is on, this positions the background image',
+        'Enum and EnumNames defines the allowed string values',
       enum: [
         'top-left',
         'top-center',
@@ -66,34 +41,15 @@ const dummyData = {
         'Bottom Right',
       ],
     },
-    media_alignment: {
-      title: 'Media Alignment',
+    string_enum_two: {
+      title: 'String Enum Two',
       type: 'string',
       enum: ['left', 'right', 'top', 'bottom'],
       enumNames: ['Left', 'Right', 'Top', 'Bottom'],
     },
-    media_padding: {
-      title: 'Media Padding',
-      type: 'string',
-      description: 'Space around the media',
-      enum: ['0', 's', 'm', 'l', 'xl', 'xxl'],
-      enumNames: [
-        'None',
-        'Small',
-        'Medium',
-        'Large',
-        'Extra Large',
-        'XX Large',
-      ],
-    },
-    media_size: {
-      title: 'Media Size',
-      type: 'string',
-      description: 'Size of media compared to content',
-      enum: ['s', 'm', 'l', 'xl'],
-      enumNames: ['Small', 'Medium', 'Large', 'Extra large'],
-    },
-  }
+
+  },
 };
+
 
 storiesOf('Schema Form', module).add('Overview', () => <SchemaForm schema={dummyData}/>);
