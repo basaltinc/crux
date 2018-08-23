4b1ddcffb7c664c1f7ddb31f42d355754b9213ab
marywarrington <mary@basalt.io>
2018-08-23 14:24:46 -0700

    remove unnecessary icons

--- bedrock/components/atoms/src/atoms.jsx
+++ bedrock/components/atoms/src/atoms.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { FaAngleDown } from 'react-icons/fa';
 
 export const Checkerboard = styled.div`
   background-image: linear-gradient(
