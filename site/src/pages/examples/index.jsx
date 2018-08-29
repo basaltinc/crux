8563ed047620a794f52060de187f97edf8c603cf
Joseph Karasek <joseph.karasek@gmail.com>
2018-08-29 09:26:06 -0700

    start landing page

---
+++ site/src/pages/examples/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ExamplesLandingPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Yoco</h1>
+      </div>
+    );
+  }
+}
+
+ExamplesLandingPage.propTypes = {};
+
+export default ExamplesLandingPage;
