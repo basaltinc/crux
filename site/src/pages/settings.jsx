67a7d079f36339102ee0aeb4ef85adfdf0e7ee92
Joseph Karasek <joseph.karasek@gmail.com>
2018-09-06 12:15:23 -0700

    schema form added to settings page

--- site/src/pages/settings.jsx
+++ site/src/pages/settings.jsx
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import { BedrockContextConsumer } from '@basalt/bedrock-core';
+import SchemaForm from '@basalt/bedrock-schema-form';
+import settingsSchema from '../../settings.schema';
 import PropTypes from 'prop-types';
 
 class SettingsPage extends Component {
   render() {
     return (
-      <div>
-        <h2>Jocomf</h2>
-      </div>
+      <BedrockContextConsumer>
+        {({settings, theme}) => (
+        <div>
+          <h4 className="eyebrow">Configuration</h4>
+          <h2>Settings</h2>
+          <hr/>
+          <br/>
+          <h3><b style={{ color: 'red'}}>Warning</b></h3>
+          <p>Editing the site settings, especially those related to source directories and server configuration, may cause this site to break. This feature is intended for development mode only.</p>
+          <br/>
+          <hr/>
+          <SchemaForm schema={settingsSchema} formData={settings}/>
+        </div>
+        )}
+      </BedrockContextConsumer>
     );
   }
 }
