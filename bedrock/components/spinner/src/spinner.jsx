48db5429c6bf99baf8b3fe56220dcf02fb11dceb
Joseph Karasek <joseph.karasek@gmail.com>
2018-09-10 10:55:58 -0700

    feat(bedrock): add error handling to Spinner
    as Spinner is used with react-loader, an error handler was added to show messages when a page fails
    to load

--- bedrock/components/spinner/src/spinner.jsx
+++ bedrock/components/spinner/src/spinner.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { CubeOne, CubeTwo, SpinnerCore } from './spinner.styles';
 
-const Spinner = props => (
-  <div>
-    <br />
-    <SpinnerCore>
-      <CubeOne />
-      <CubeTwo />
-    </SpinnerCore>
-    <br />
-    {props.text && <h2>{props.text}</h2>}
-  </div>
-);
+const Spinner = props => {
+  if (props.error) {// used by `react-loadable`
+    throw new Error(props.error);
+  }
+  return (
+    <div>
+      <br />
+      <SpinnerCore>
+        <CubeOne />
+        {/*@todo the added styles below are for a bug introduced in v4 of sytled components*/}
+        <CubeTwo style={{ animationDelay: '-0.9s' }} />
+      </SpinnerCore>
+      <br />
+      {props.text && <h2>{props.text}</h2>}
+    </div>
+  );
+};
 
 Spinner.propTypes = {
   text: PropTypes.string,
