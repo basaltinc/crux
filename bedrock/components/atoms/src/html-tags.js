f46f7dde8be059802500c188b7669bbe4381a02b
Joseph Karasek <joseph.karasek@gmail.com>
2018-09-11 10:54:25 -0700

    break apart atoms directory into sub pieces

---
+++ bedrock/components/atoms/src/html-tags.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+export const BlockQuoteWrapper = styled.blockquote`
+  border: ${props => props.theme.blockquote.border};
+  border-left: ${props => props.theme.blockquote['border-left']};
+  padding: ${props => props.theme.blockquote.padding};
+  position: relative;
+  margin: ${props => props.theme.blockquote.margin};
+  footer {
+    font-size: 0.8em;
+    color: ${props => props.theme.colors.color.gray.light};
+    margin-top: 0.5rem;
+  }
+  ::before {
+    content: '\\201C'; /*Unicode for Left Double Quote*/
+    width: ${props => props.theme.blockquote.glyph.width};
+    /*Font*/
+    font-family: ${props => props.theme.blockquote.glyph.font};
+    font-size: ${props => props.theme.blockquote.glyph.size};
+    opacity: ${props => props.theme.blockquote.glyph.opacity};
+    font-weight: bold;
+    color: ${props => props.theme.blockquote.glyph.color};
+
+    /*Positioning*/
+    position: absolute;
+    left: 0px;
+    top: -30px;
+  }
+  ::after {
+    content: '\\201D'; /*Unicode for Right Double Quote*/
+    width: ${props => props.theme.blockquote.glyph.width};
+    font-family: ${props => props.theme.blockquote.glyph.font};
+    font-size: ${props => props.theme.blockquote.glyph.size};
+    opacity: ${props => props.theme.blockquote.glyph.opacity};
+    font-weight: bold;
+    color: ${props => props.theme.blockquote.glyph.color};
+    position: absolute;
+    right: 10px;
+    bottom: -85px;
+  }
+`;
+
+export const Details = styled.details`
+  padding: 7px 0;
+  border-top: solid 1px ${props => props.theme.colors.color.gray.dark};
+  border-bottom: solid 1px ${props => props.theme.colors.color.gray.dark};
+  margin-bottom: 10px;
+  > summary {
+    font-weight: bold;
+    outline: none;
+    user-select: none;
+    font-size: 1.1rem;
+    &:hover {
+      cursor: pointer;
+    }
+    &:active,
+    &:focus {
+      outline: none;
+    }
+  }
+`;
