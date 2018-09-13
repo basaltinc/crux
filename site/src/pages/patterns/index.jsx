c414f5ac072b8bce1b5a6d8ce38decd63238a3f2
Joseph Karasek <joseph.karasek@gmail.com>
2018-09-13 10:02:01 -0700

    refactor(site): refactor Mary's awesome work
    refactor component page filters to work with only the object of form data, remove visibleUses and
    visibaleStatus array

--- site/src/pages/patterns/index.jsx
+++ site/src/pages/patterns/index.jsx
@@ -4,14 +4,57 @@ import SchemaForm from '@basalt/bedrock-schema-form';
 import PatternGrid from '@basalt/bedrock-pattern-grid';
 import { apiUrlBase } from '../../../config';
 
+const statusesSchema = {
+  $schema: 'http://json-schema.org/draft-07/schema',
+  title: 'Status',
+  type: 'object',
+  // description:
+  // 'Denotes whether the component is published (ready) or a draft.',
+  properties: {
+    ready: {
+      title: 'Ready',
+      type: 'boolean',
+      description: 'Denotes that the component is ready and published',
+    },
+    draft: {
+      title: 'Draft',
+      type: 'boolean',
+      description: 'Denotes that the component is a draft, and not published',
+    },
+  },
+};
+
+const usesSchema = {
+  $schema: 'http://json-schema.org/draft-07/schema',
+  title: 'Uses',
+  type: 'object',
+  // description: 'Denotes possible uses for each component.',
+  properties: {
+    inComponent: {
+      title: 'In Component',
+      type: 'boolean',
+      description: 'Used in a component',
+    },
+    inSlice: {
+      title: 'In Slice',
+      type: 'boolean',
+      description: 'Used in slices',
+    },
+    inGrid: {
+      title: 'In Grid',
+      type: 'boolean',
+      description: 'Used in a grid',
+    },
+  },
+};
+
 class PatternsPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      patterns: [],
       isReady: false,
-      visibleStatuses: [],
-      // @todo programatically set statuses and users
+      patterns: [],
+      visiblePatterns: [],
       statuses: {
         ready: true,
         draft: true,
@@ -21,8 +64,6 @@ class PatternsPage extends Component {
         inGrid: true,
         inSlice: true,
       },
-      visibleUses: [],
-      visiblePatterns: [],
     };
     this.handleChangeStatuses = this.handleChangeStatuses.bind(this);
     this.handleChangeUses = this.handleChangeUses.bind(this);
@@ -34,76 +75,49 @@ class PatternsPage extends Component {
       .fetch(`${apiUrlBase}/patterns/component`)
       .then(res => res.json())
       .then(patterns => {
-        const { statuses, uses, visibleStatuses, visibleUses } = this.state;
-
-        Object.keys(statuses).forEach(value => {
-          if (statuses[value] !== false) {
-            visibleStatuses.push(value);
-          }
-        });
-
-        Object.keys(uses).forEach(value => {
-          if (uses[value] !== false) {
-            visibleUses.push(value);
-          }
-        });
-
         this.setState({
-          patterns,
           isReady: true,
-          visibleStatuses,
-          visibleUses,
+          patterns,
           visiblePatterns: patterns,
         });
-        this.updateVisiblePatterns();
       });
   }
 
   updateVisiblePatterns() {
-    const { patterns, visibleUses, visibleStatuses } = this.state;
+    const { patterns } = this.state;
 
     const visibleItems = patterns
       .filter(item => item.id !== 'site-footer')
       .filter(item => item.id !== 'site-header')
-      .filter(item => visibleStatuses.includes(item.status))
-      .filter(item => item.uses.some(use => visibleUses.includes(use)));
+      .filter(item => {
+        const visibleStatuses = Object.keys(this.state.statuses).filter(
+          key => this.state.statuses[key],
+        );
+        return visibleStatuses.some(status => item.status === status);
+      })
+      .filter(item => {
+        const visibleUses = Object.keys(this.state.uses).filter(
+          key => this.state.uses[key],
+        );
+        return item.uses.some(use => visibleUses.includes(use));
+      });
 
     this.setState({
       visiblePatterns: visibleItems,
     });
   }
 
-  // @todo there has to be a way to make these two handleChange functions into one
-
   handleChangeStatuses({ formData }) {
-    this.setState(
-      {
-        statuses: formData,
-        visibleStatuses: [],
-      },
-      () =>
-        Object.keys(this.state.statuses).forEach(value => {
-          if (this.state.statuses[value] !== false) {
-            this.state.visibleStatuses.push(value);
-          }
-        }),
-    );
+    this.setState({
+      statuses: formData,
+    });
     this.updateVisiblePatterns();
   }
 
   handleChangeUses({ formData }) {
-    this.setState(
-      {
-        uses: formData,
-        visibleUses: [],
-      },
-      () =>
-        Object.keys(this.state.uses).forEach(value => {
-          if (this.state.uses[value] !== false) {
-            this.state.visibleUses.push(value);
-          }
-        }),
-    );
+    this.setState({
+      uses: formData,
+    });
     this.updateVisiblePatterns();
   }
 
@@ -111,51 +125,6 @@ class PatternsPage extends Component {
     if (!this.state.isReady) {
       return <Spinner />;
     }
-
-    const statusesSchema = {
-      $schema: 'http://json-schema.org/draft-07/schema',
-      title: 'Status',
-      type: 'object',
-      // description:
-      // 'Denotes whether the component is published (ready) or a draft.',
-      properties: {
-        ready: {
-          title: 'Ready',
-          type: 'boolean',
-          description: 'Denotes that the component is ready and published',
-        },
-        draft: {
-          title: 'Draft',
-          type: 'boolean',
-          description:
-            'Denotes that the component is a draft, and not published',
-        },
-      },
-    };
-
-    const usesSchema = {
-      $schema: 'http://json-schema.org/draft-07/schema',
-      title: 'Uses',
-      type: 'object',
-      // description: 'Denotes possible uses for each component.',
-      properties: {
-        inComponent: {
-          title: 'In Component',
-          type: 'boolean',
-          description: 'Used in a component',
-        },
-        inSlice: {
-          title: 'In Slice',
-          type: 'boolean',
-          description: 'Used in slices',
-        },
-        inGrid: {
-          title: 'In Grid',
-          type: 'boolean',
-          description: 'Used in a grid',
-        },
-      },
-    };
 
     return (
       <div>
