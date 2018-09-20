e5eebd1663bd647263920832e09f9e39e9695eb0
Jake Love <jake@basalt.io>
2018-09-20 14:12:51 -0700

    Updated existing Smart Grid instances to match new prop architecture

--- bedrock/components/pattern-grid/src/pattern-grid.styles.js
+++ bedrock/components/pattern-grid/src/pattern-grid.styles.js
@@ -1,16 +1,18 @@
 import styled from 'styled-components';
-import { SmartGrid } from '@basalt/bedrock-smart-grid';
 
 export const PatternGridItem = styled.div`
   background: #fff;
   position: relative;
+  width: 100%;
   transition: ${props => props.theme.transition.all};
-  ${SmartGrid}:hover & {
-    filter: blur(0.75px);
-  }
-  &:hover {
-    filter: blur(0px) !important;
-    z-index: 100;
+  @media (min-width: ${props => props.theme.breakpoint.medium}) {
+    .pattern-grid-wrapper:hover & {
+      filter: blur(0.75px);
+    }
+    &:hover {
+      filter: blur(0px) !important;
+      z-index: 100;
+    }
   }
   > a:link,
   > a:visited {
@@ -29,15 +31,17 @@ export const PatternGridItem = styled.div`
     position: relative;
     z-index: 100;
   }
-  > a:hover {
-    position: absolute;
-    background: #fff;
-    border-radius: ${props => props.theme.border.radius};
-    border-color: ${props => props.theme.colors.type.component.base};
-    transform: scale(1.05);
-    width: 100%;
-    height: auto;
-    z-index: 200;
+  @media (min-width: ${props => props.theme.breakpoint.medium}) {
+    > a:hover {
+      position: absolute;
+      background: #fff;
+      border-radius: ${props => props.theme.border.radius};
+      border-color: ${props => props.theme.colors.type.component.base};
+      transform: scale(1.15);
+      width: 100%;
+      height: auto;
+      z-index: 200;
+    }
   }
 `;
 
@@ -68,20 +72,23 @@ export const PatternGridItemDescription = styled.div`
   font-size: ${props => props.theme.fonts.sizes.s};
   color: #000;
   font-style: italic;
-  padding: ${props => props.theme.spacing.l} 0 ${props => props.theme.spacing.l};
+  padding: ${props => props.theme.spacing.m} 0 ${props => props.theme.spacing.m};
   max-width: 250px;
   margin: 0 auto;
-  opacity: 0;
-  height: 0;
-  pointer-events: none;
-  transition: all 0.1s ease-in-out;
-  transform: scale(0.75);
-  position: absolute;
-
-  ${PatternGridItem} > a:hover & {
-    opacity: 1;
-    transform: scale(1);
-    height: auto;
-    position: relative;
+  @media (min-width: ${props => props.theme.breakpoint.medium}) {
+    padding: ${props => props.theme.spacing.l} 0
+      ${props => props.theme.spacing.l};
+    opacity: 0;
+    height: 0;
+    pointer-events: none;
+    transform: scale(0.75);
+    position: absolute;
+    transition: all 0.1s ease-in-out;
+    ${PatternGridItem} > a:hover & {
+      opacity: 1;
+      transform: scale(1);
+      height: auto;
+      position: relative;
+    }
   }
 `;
