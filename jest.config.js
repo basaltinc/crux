e1700e8db815a1475add7e34e67025d4c9643ece
Evan Lovely <evanlovely@gmail.com>
2018-11-14 21:32:40 -0800

    cleaning up old our-design-system files

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  setupFiles: ['<rootDir>/jest.init.js'],
-  testPathIgnorePatterns: ['node_modules', 'vendor', 'public2', 'dist'],
-  transform: {
-    '^.+\\.jsx$': 'babel-jest',
-    '^.+\\.js$': 'babel-jest',
-  },
-  verbose: true,
-};
