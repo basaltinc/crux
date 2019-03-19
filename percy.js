5543b55f414ce748a19e7a24c5daf4c0289e99f7
Evan Lovely <evanlovely@gmail.com>
2019-03-19 16:08:10 -0700

    ci: loop through ALL templates and demoUrls for Percy

--- percy.js
+++ percy.js
@@ -14,6 +14,7 @@ if (!baseUrl) {
 const bedrock = bootstrapFromConfigFile('./bedrock.config.js');
 
 const { patterns } = bedrock;
+const allPatterns = patterns.getPatterns();
 
 async function go() {
 // Start a Puppeteer instance.
@@ -21,32 +22,27 @@ async function go() {
     headless: true,
     args: [ '--single-process' ],
   });
-
-  const allPatterns = patterns.getPatterns();
+  const page = await browser.newPage();
 
   for (const pattern of allPatterns) {
+    // const [ template ] = pattern.templates;
+    // const [ demoUrl ] = template.demoUrls;
+    // const fullUrl = resolve(baseUrl, demoUrl);
 
-    const [ template ] = pattern.templates;
-    const [ demoUrl ] = template.demoUrls;
-    const id = `${pattern.id}-${template.id}-0`;
-    const fullUrl = resolve(baseUrl, demoUrl);
-    console.log(id);
-    const page = await browser.newPage();
-
-    // Load app.
-    await page.goto(fullUrl);
-    await percySnapshot(page, id);
-    console.log(`done: ${id}`);
-    // pattern.templates.forEach(template => {
-    //   /** @type {BedrockPatternTemplate} */
-    //   const t = template;
-    //   t.demoUrls.forEach(demoUrl => {
-    //     const fullUrl = resolve(baseUrl, demoUrl);
-    //     // console.log(`${pattern.id}-${t.id}`);
-    //     // console.log(fullUrl);
-    //     // console.log('-----');
-    //   })
-    // })
+    for (const template of templates) {
+      /** @type {BedrockPatternTemplate} */
+      const t = template;
+      let i = 0;
+      for (const demoUrl of t.demoUrls) {
+        i++;
+        const id = `${pattern.id}-${template.id}-${i}`;
+        console.log(id);
+        const fullUrl = resolve(baseUrl, demoUrl);
+        await page.goto(fullUrl);
+        await percySnapshot(page, id);
+        console.log(`done: ${id}`);
+      }
+    }
   }
 
   console.log('all done');
