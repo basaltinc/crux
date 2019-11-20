b3e588581d923b6a67f89d1c909ce21802a4fe60
Brandon Reid <brandonreidak@gmail.com>
2019-11-20 14:22:29 -0800

    feat: add content divider

---
+++ _patterns/atoms/content-divider/content-divider-schema.js
@@ -0,0 +1,7 @@
+module.exports = {
+  $schema: 'http://json-schema.org/draft-07/schema',
+  title: 'Content Divider',
+  type: 'object',
+  description: "Full width divider for separating content on a page.",
+  properties: {},
+};
--- _patterns/molecules/anchored-blurb/knapsack.pattern.js
+++ _patterns/molecules/anchored-blurb/knapsack.pattern.js
@@ -13,7 +13,7 @@ module.exports = {
         {
           header: 'Header Text',
           headingLevel: 2,
-          body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+          body: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>',
           has_background: true,
           ctas: [
             {
