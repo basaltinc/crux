5efbf72c8695849f18b9199969666c3c544ff883
marywarrington <mary@basalt.io>
2019-11-27 11:56:53 -0800

    fix: adds dark text option for anchored blurb

--- _patterns/molecules/anchored-blurb/anchored-blurb-schema.js
+++ _patterns/molecules/anchored-blurb/anchored-blurb-schema.js
@@ -6,7 +6,7 @@ const {
   extraClasses,
   link_alt,
   link_url,
-  link_openNewTab
+  link_openNewTab,
 } = buttonSchema.properties;
 
 const ctaSchema = {
@@ -15,41 +15,45 @@ const ctaSchema = {
   extraClasses,
   link_alt,
   link_url,
-  link_openNewTab
+  link_openNewTab,
 };
 
 module.exports = {
   $schema: 'http://json-schema.org/draft-07/schema',
   title: 'Anchored Blurb',
   type: 'object',
-  description: "Typically displayed in a hero banner, a header and paragraph combo box anchored by a left citrine bar.",
+  description:
+    'Typically displayed in a hero banner, a header and paragraph combo box anchored by a left citrine bar.',
   required: ['header'],
   properties: {
     header: {
       title: 'Header Text',
       type: 'string',
-      description:
-        'Larger header text.',
+      description: 'Larger header text.',
     },
     headingLevel: {
       title: 'Heading Level',
       type: 'number',
       default: 2,
       description:
-        'Set the heading level (1-6) so that the heading fits properly in the dom header tree.'
+        'Set the heading level (1-6) so that the heading fits properly in the dom header tree.',
     },
     body: {
       title: 'Body Text',
       type: 'string',
-      description:
-        'Paragraph text below the header.'
+      description: 'Paragraph text below the header.',
     },
     hasBackground: {
       title: 'Has Background',
       type: 'boolean',
       default: false,
-      description:
-        'Whether a semi-transparent dark background should be used.'
+      description: 'Whether a semi-transparent dark background should be used.',
+    },
+    hasDarkText: {
+      title: 'Has Dark Text',
+      type: 'boolean',
+      default: false,
+      description: 'If true, white text is turned to black.',
     },
     maxWidth: {
       title: 'Max Width (px)',
@@ -62,7 +66,7 @@ module.exports = {
       description: 'List of calls to action as link buttons.',
       items: {
         ...buttonSchema,
-        properties: ctaSchema
+        properties: ctaSchema,
       },
     },
     extraClasses: {
